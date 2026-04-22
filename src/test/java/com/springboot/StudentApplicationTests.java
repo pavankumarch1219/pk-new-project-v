1bf26a4486c13283fc513aa40c98b4c6475d7e71
pavankumarch1219 <pavankumarch29@gmail.com>
2026-04-22 14:14:46 +0530

    Add initial test class for StudentApplication

---
+++ src/test/java/com/springboot/StudentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
